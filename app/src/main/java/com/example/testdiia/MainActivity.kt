4da6252cc64a07700222ccfdf5ddb1601edd5fde
naf <avtomax12345@gmail.com>
2023-12-13 18:58:38 +0200

    diia auth

---
+++ app/src/main/java/com/example/testdiia/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.example.testdiia
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var webView: WebView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        webView = findViewById<WebView>(R.id.webView)
+
+        setupWebView()
+
+        val website = "https://auto.ria.com"
+        webView.loadUrl(website)
+    }
+
+
+    private fun setupWebView() {
+        webView.webViewClient = object : WebViewClient() {
+            override fun shouldOverrideUrlLoading(
+                view: WebView?,
+                request: WebResourceRequest?
+            ): Boolean {
+                return false
+            }
+        }
+        // user agent for android - imitate browser
+        val userAgent =
+            "Mozilla/5.0 (Linux; Android 14) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.6099.43 Mobile Safari/537.36"
+        val settings = webView.settings
+        settings.javaScriptEnabled = true
+        settings.domStorageEnabled = true
+        settings.userAgentString = userAgent
+
+
+    }
+    override fun onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack()
+        } else {
+            onBackPressedDispatcher.onBackPressed()
+        }
+    }
+}
